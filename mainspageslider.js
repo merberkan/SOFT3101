f52b3efa1a0d89958e291c014d65715e80c43f86
Baran Dolapci <72374750+bdolapci@users.noreply.github.com>
2020-12-25 21:42:01 +0300

    MainPage update

--- mainspageslider.js
+++ mainspageslider.js
@@ -1,10 +1,10 @@
 window.onload = function(){
-    var div=document.getElementById("deneme");
+    var div=document.getElementById("textsliderjss");
     var slideImage = document.getElementById("slide-image");
     var h1=document.getElementById("tet");
     var images = ["images/concertsmoke.jpg","images/basketball2.jpg","images/theater.jpg","images/football.jpg"];
-    var text =["deneme1","deneme2","deneme3","deneme4"];
-    var text2 =["deneme3","deneme4","deneme53","deneme46"];
+    var text =["Senin Dünyan Senin Kuralların","Oyunun Kurallarını Sen Belirle"," Eğlenceyi En Yakından Takip Et","Heyecanı Sonuna Kadar Yaşa"];
+    var text2 =["En Sevdiğin Etkinlikler Snollda","En Sevdiğin Etkinlikler Snollda","En Sevdiğin Etkinlikler Snollda","En Sevdiğin Etkinlikler Snollda"];
     var counter = 1;
     div.innerHTML="";
     slideImage.src = images[0];
